cf6b5bb1311afd0fa56489d127f219820cccf333
jan.kaemmerer@swisslog.com <jan.kaemmerer@swisslog.com>
2022-05-30 09:50:20 +0200

    second homework

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='mlops_zoomcamp_homework',
+    version='',
+    packages=[''],
+    url='',
+    license='',
+    author='Jan Weber',
+    author_email='',
+    description=''
+)
